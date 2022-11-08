878ba96c1452c5d0d805a91e7d5199fb2a63f9a6
majharul224 <majharulislam2225@gmail.com>
2022-11-08 15:42:30 +0600

    add a router

--- src/component/Router/Routes/Routes.js
+++ src/component/Router/Routes/Routes.js
@@ -1,35 +1,35 @@
-// import React from 'react';
-// import Main from '../../LayOut/Main';
-// import Homes from '../../Pages/Home/Homes/Homes';
-// import Login from '../../Pages/Login/Login';
-// import NotFound from '../../Pages/NotFound/NotFound';
-// import Signup from '../../Pages/SignUp/Signup';
+import React from 'react';
+import Main from '../../LayOut/Main';
+import Homes from '../../Pages/Home/Homes/Homes';
+import Login from '../../Pages/Login/Login';
+import NotFound from '../../Pages/NotFound/NotFound';
+import Signup from '../../Pages/SignUp/Signup';
 
-// const { createBrowserRouter } = require("react-router-dom");
+const { createBrowserRouter } = require("react-router-dom");
 
-// const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element: <Main></Main>,
-//         children: [
-//             {
-//                 path: '/',
-//                 element: <Homes></Homes>
-//             },
-//             {
-//                 path: '/login',
-//                 element: <Login></Login>
-//             },
-//             {
-//                 path: '/signup',
-//                 element: <Signup></Signup>
-//             },
-//         ]
-//     },
-//     {
-//         path: '*',
-//         element: <NotFound></NotFound>
-//     }
-// ]);
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Homes></Homes>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound></NotFound>
+    }
+]);
 
-// export default router;
+export default router;
