d5f9c2b02e3baa20277afb5ace72b2741f04fac2
majharul224 <majharulislam2225@gmail.com>
2022-11-09 23:38:15 +0600

    added a private page

--- src/component/Router/PrivateRouter/PrivateRouter.js
+++ src/component/Router/PrivateRouter/PrivateRouter.js
@@ -1,31 +1,31 @@
 
-// import React, { useContext } from "react";
-// import { Navigate, useLocation } from "react-router-dom";
-// import { Vortex } from "react-loader-spinner";
-// import { AuthContext } from "../../Context/AuthProvider";
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { Vortex } from "react-loader-spinner";
+import { AuthContext } from "../../Context/AuthProvider";
 
-// const PrivateRouter = ({ children }) => {
-//   const { user, loading } = useContext(AuthContext);
-//   const location = useLocation();
-//   if (loading) {
-//     return (
-//       <div style={{ marginLeft: "45%" }}>
-//         <Vortex
-//           visible={true}
-//           height="100"
-//           width="100"
-//           ariaLabel="vortex-loading"
-//           wrapperStyle={{}}
-//           wrapperClass="vortex-wrapper"
-//           colors={["red", "green", "blue", "yellow", "orange", "purple"]}
-//         />
-//       </div>
-//     );
-//   }
-//   if (user) {
-//     return children;
-//   }
-//   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
-// };
+const PrivateRouter = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return (
+      <div style={{ marginLeft: "45%" }}>
+        <Vortex
+          visible={true}
+          height="100"
+          width="100"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={["red", "green", "blue", "yellow", "orange", "purple"]}
+        />
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
 
-// export default PrivateRouter;
+export default PrivateRouter;
