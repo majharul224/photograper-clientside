cf2388209c8d619aa2623e90e7d963b58f002d47
majharul224 <majharulislam2225@gmail.com>
2022-11-10 14:33:30 +0600

    added a carousel page

--- src/component/Pages/Home/Carousel/Carousel.js
+++ src/component/Pages/Home/Carousel/Carousel.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import caro1 from '../../../../images/ima/caro.png'
-// import caro2 from '../../../../images/ima/caro1.png'
-// import caro3 from '../../../../images/ima/caro2.png'
-// import caro4 from '../../../../images/ima/caro3.png'
-// import caro5 from '../../../../images/ima/caro4.png'
+import React from 'react';
+import caro1 from '../../../../images/ima/caro.png'
+import caro2 from '../../../../images/ima/caro1.png'
+import caro3 from '../../../../images/ima/caro2.png'
+import caro4 from '../../../../images/ima/caro3.png'
+import caro5 from '../../../../images/ima/caro4.png'
 
-// const Carousel = () => {
-//     return (
-//         <div className="relative w-full flex gap-4 py-6 my-12  overflow-x-auto">
-//             <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro1} alt="Image 1" />
-//             <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro2} alt="Image 2" />
-//             <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro3} alt="Image 3" />
-//             <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro4}alt="Image 4" />
-//             <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro5} alt="Image 5" />
-//         </div>
+const Carousel = () => {
+    return (
+        <div className="relative w-full flex gap-4 py-6 my-12  overflow-x-auto">
+            <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro1} alt="Image 1" />
+            <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro2} alt="Image 2" />
+            <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro3} alt="Image 3" />
+            <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro4}alt="Image 4" />
+            <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src={caro5} alt="Image 5" />
+        </div>
 
-//     );
-// };
+    );
+};
 
-// export default Carousel;
+export default Carousel;
